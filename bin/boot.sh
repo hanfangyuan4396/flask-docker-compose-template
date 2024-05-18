d462aee9e1a877ee371e6fc4f7595569c65df31b
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-05-18 23:31:56 +0800

    feat: merge gunicorn log to app log

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,1 +1,1 @@
-gunicorn -w $WORKER_NUM --access-logfile gunicorn.log --error-logfile gunicorn.log --preload app:app -b 0.0.0.0:$PORT
+gunicorn -w $WORKER_NUM --access-logfile app.log --error-logfile app.log --preload app:app -b 0.0.0.0:$PORT
